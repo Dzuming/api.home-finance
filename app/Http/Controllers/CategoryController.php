6e6f81dffb7bfe248b811270234d124be041ec34
Dzuming <dawidpoliszak@op.pl>
2018-02-04 08:40:58 +0100

    Send categories in response json

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -7,6 +7,6 @@ use App\Models\Category;
 class CategoryController extends Controller
 {
     public function getCategories() {
-        return Category::all();
+        return \Response::json(Category::all(), 200);
     }
 }
