b5e3df5a35c4f1635e1b9e0c92f25b3d60d78154
Dzuming <dawidpoliszak@op.pl>
2018-04-07 07:40:58 +0200

    Add hash to password

--- src/lib/passport.js
+++ src/lib/passport.js
@@ -1,20 +1,29 @@
 import passport from 'passport';
 import Strategy from 'passport-local';
-import { getUserByEmailPassword } from '../models/user';
+import { getUserByEmail } from '../models/user';
+import bcrypt from 'bcrypt';
+import logger from './logger';
 
 const authenticate = passport.use(new Strategy({
     usernameField: 'email',
     passwordField: 'password'
   },
   function (email, password, cb) {
-    //this one is typically a DB call. Assume that the returned user object is pre-formatted and ready for storing in JWT
-    getUserByEmailPassword(email, password).then(user => {
-      if (!user) {
+    const hash = bcrypt.hashSync(password, 10);
+    bcrypt.compare(password, hash, function(err, res) {
+      if(res) {
+        getUserByEmail(email).then(user => {
+          logger.info(user)
+          if (!user) {
+            return cb(null, false, {message: 'Incorrect email or password.'});
+          }
+          return cb(null, user, {message: 'Logged In Successfully'});
+        })
+          .catch(err => cb(err));
+      } else {
         return cb(null, false, {message: 'Incorrect email or password.'});
       }
-      return cb(null, user, {message: 'Logged In Successfully'});
-    })
-      .catch(err => cb(err));
+    });
   }
 ));
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,3 @@
 import model from '../../db/models';
 
-export const getUserByEmailPassword = (email, password) => model.User.findOne({where: {email, password}});
-
 export const getUserByEmail = (email) => model.User.findOne({where: {email}});
