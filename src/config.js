53aaefe9af907a792eccf04b7102e4076d978fdd
Dzuming <dawidpoliszak@op.pl>
2018-04-24 19:40:31 +0200

    Add env variables

--- src/config.js
+++ src/config.js
@@ -3,13 +3,13 @@ dotenv.config();
 
 export default {
   db: {
-    host: 'root',
-    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    user: 'root',
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
   },
   port: 8000,
   bodyLimit: '100kb',
   corsHeaders: ['Link'],
-  secret: process.env.SECRET,
+  secret: 'dd',
 };
